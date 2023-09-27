42fc7bd19e58303604ab682d4394b3d2259e5b32
Deividhsn <deividhsn@gmail.com>
2023-09-27 15:52:55 -0300

    att

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,36 +6,43 @@ window.addEventListener("scroll", function () {
 
 // Carroussel
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 10,
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 1,
+//     spaceBetween: 10,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     breakpoints: {
+//       640: {
+//         slidesPerView: 2,
+//         spaceBetween: 20,
+//       },
+//       768: {
+//         slidesPerView: 4,
+//         spaceBetween: 40,
+//       },
+//       1024: {
+//         slidesPerView: 3,
+//         spaceBetween: 50,
+//       },
+//     },
+//   });
+
+
+  var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
     pagination: {
       el: ".swiper-pagination",
-      clickable: true,
-    },
-    breakpoints: {
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 4,
-        spaceBetween: 40,
-      },
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 50,
-      },
     },
   });
-
-//   GO ANDE BACK ANIMATION
-
-// $(".swiper-slide").mouseenter(function(){
-//     $(".swiper-slide div").addClass("go"); 
-// });
-
-// $(".swiper-slide").mouseout(function(){
-//     $(".swiper-slide div").toggleclass("go");
-// });
-
