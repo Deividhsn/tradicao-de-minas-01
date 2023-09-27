08995907a7809d65182c75252f322045a4e02e13
Deividhsn <deividhsn@gmail.com>
2023-09-27 11:57:00 -0300

    Atualização

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,38 @@ window.addEventListener("scroll", function () {
     header.classList.toggle("alterHeader", window.scrollY > 0);
 
 });
+
+// Carroussel
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
+  });
+
+//   GO ANDE BACK ANIMATION
+
+$(".swiper-slide").mouseenter(function(){
+    $(".swiper-slide div").addClass("go"); 
+});
+
+$(".swiper-slide").mouseout(function(){
+    $(".swiper-slide div").toggleclass("go");
+});
