452526d60eb259a61a57d75c50baee151e038505
Deividhsn <deividhsn@gmail.com>
2023-09-28 12:39:57 -0300

    Atualização Section-all-produtos

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,41 +6,53 @@ window.addEventListener("scroll", function () {
 
 // Carroussel
 
-// var swiper = new Swiper(".mySwiper", {
-//     slidesPerView: 1,
-//     spaceBetween: 10,
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//     breakpoints: {
-//       640: {
-//         slidesPerView: 2,
-//         spaceBetween: 20,
-//       },
-//       768: {
-//         slidesPerView: 4,
-//         spaceBetween: 40,
-//       },
-//       1024: {
-//         slidesPerView: 3,
-//         spaceBetween: 50,
-//       },
-//     },
-//   });
+ var swiper = new Swiper(".mySwiper", {
+   
+     slidesPerView: 1,
+     spaceBetween: 10,
+     loop: true,
+     autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+      
+      
+    },
+     pagination: {
+       el: ".swiper-pagination",
+       clickable: true,
+       
+     },
+     breakpoints: {
+       640: {
+         slidesPerView: 2,
+         spaceBetween: 20,
+       },
+       768: {
+         slidesPerView: 2,
+         spaceBetween: 40,
+       },
+       1024: {
+         slidesPerView: 3,
+         spaceBetween: 50,
+       },
+     },
+   });
 
 
-  var swiper = new Swiper(".mySwiper", {
+  var swiper = new Swiper(".my", {
     effect: "coverflow",
     grabCursor: true,
     centeredSlides: true,
-    slidesPerView: "auto",
+    slidesPerView: "1.5",
+    loop: true,
+   
     coverflowEffect: {
       rotate: 50,
       stretch: 0,
       depth: 100,
       modifier: 1,
-      slideShadows: true,
+      slideShadows: false,
+    
     },
     pagination: {
       el: ".swiper-pagination",
