be16fe2a5a638acf86dbd54cdedcc24d40c39dba
Deividhsn <deividhsn@gmail.com>
2023-10-10 13:30:17 -0300

    Atualização

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,49 +1,22 @@
 let header = document.querySelector("header");
 window.addEventListener("scroll", function () {
     header.classList.toggle("alterHeader", window.scrollY > 0);
-
 });
 
 // CLICK MENU RESPONSIVO
 $(".openMenu").click(function(){  
   $("header ul").toggleClass("display"); 
-
 })
-// Carroussel
 
-  var swiper = new Swiper(".my", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    spaceBetween: 100,
-    slidesPerView: "1",
-    loop: true,
-    // autoplay: {
-    //        delay: 5000,
-    //         disableOnInteraction: false,  
-    //      },
-   
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+// BUTTON PRODUCTS
+$(document).ready(function () {
+  $(".sale").hide();
+  $(".aboutMore").click(function () {
+      $(".sale").hide();
+      $(this).closest(".products").find(".sale").show();
   });
+});
 
-
-// Ajuste button para responsividade
-
-$(".aboutMore").click(function(){
-  $(".pictures button").toggleClass("testando")
-
+$(".more").click(function(){
+  $(".sale").css("display","none");
 })
